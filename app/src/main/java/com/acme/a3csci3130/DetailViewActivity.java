dcd7cd9f021149337a807ea0a47c5dc3ffaa9378
vibar <victor.bares@ensta-bretagne.org>
2018-03-07 16:54:26 -0400

    Update and erase functionnality
    - Write the delete functionnality
    - Write the erase functionnality

--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -6,16 +6,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+
 public class DetailViewActivity extends Activity {
 
     private EditText nameField, emailField;
     Contact receivedPersonInfo;
+    private MyApplicationData appState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
         receivedPersonInfo = (Contact)getIntent().getSerializableExtra("Contact");
+
+        appState = ((MyApplicationData) getApplicationContext());
 
         nameField = (EditText) findViewById(R.id.name);
         emailField = (EditText) findViewById(R.id.email);
@@ -27,11 +32,23 @@ public class DetailViewActivity extends Activity {
     }
 
     public void updateContact(View v){
-        //TODO: Update contact funcionality
+        String name = nameField.getText().toString();
+        String email = emailField.getText().toString();
+        Contact person = new Contact(receivedPersonInfo.uid, name, email);
+        appState.firebaseReference.child(receivedPersonInfo.uid).setValue(person);
+        finish();
     }
 
     public void eraseContact(View v)
     {
-        //TODO: Erase contact functionality
+        /*String personID = appState.firebaseReference.getKey(receivedPersonInfo.name);
+
+        String name = nameField.getText().toString();
+        String email = emailField.getText().toString();
+        Contact person = new Contact(personID, name, email);
+
+        appState.firebaseReference.child(personID).setValue(person);*/
+        appState.firebaseReference.child(receivedPersonInfo.uid).removeValue();
+        finish();
     }
 }
