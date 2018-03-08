797fb22a7c56b20d53cee75e7adce771be6c86dd
vibar <victor.bares@ensta-bretagne.org>
2018-03-08 17:50:38 -0400

    Changing the detailviewactivity
    -Adapting the detail view activity
    - adding a done button on the keyboard

--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -10,7 +10,8 @@ import com.google.firebase.database.DatabaseReference;
 
 public class DetailViewActivity extends Activity {
 
-    private EditText nameField, emailField;
+    private EditText nameField, emailField,bussnumberField, addrField,primbusField,proTerrField;
+
     Contact receivedPersonInfo;
     private MyApplicationData appState;
 
@@ -24,17 +25,30 @@ public class DetailViewActivity extends Activity {
 
         nameField = (EditText) findViewById(R.id.name);
         emailField = (EditText) findViewById(R.id.email);
+        bussnumberField= (EditText) findViewById(R.id.bussnumber);
+        primbusField = (EditText) findViewById(R.id.primbus);
+        addrField = (EditText) findViewById(R.id.addr);
+        proTerrField = (EditText) findViewById(R.id.proTerr);
 
         if(receivedPersonInfo != null){
             nameField.setText(receivedPersonInfo.name);
             emailField.setText(receivedPersonInfo.email);
+            bussnumberField.setText(receivedPersonInfo.bussnumber);
+            primbusField.setText(receivedPersonInfo.primbus);
+            addrField.setText(receivedPersonInfo.addr);
+            proTerrField.setText(receivedPersonInfo.proTerr);
         }
     }
 
     public void updateContact(View v){
+        String personID = appState.firebaseReference.push().getKey();
         String name = nameField.getText().toString();
         String email = emailField.getText().toString();
-        Contact person = new Contact(receivedPersonInfo.uid, name, email);
+        String bussnumber = bussnumberField.getText().toString();
+        String primbus = primbusField.getText().toString();
+        String addr = addrField.getText().toString();
+        String proTerr = proTerrField.getText().toString();
+        Contact person = new Contact(personID, name, email,bussnumber,primbus,addr,proTerr);
         appState.firebaseReference.child(receivedPersonInfo.uid).setValue(person);
         finish();
     }
